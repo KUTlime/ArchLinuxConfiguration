a47db9667ee21a98f527c064937152574d91fc29
KUTlime <kutlime@gmail.com>
2026-02-19 10:37:33 +0100

    Update installation packages

--- src/installSoftware.sh
+++ src/installSoftware.sh
@@ -3,7 +3,6 @@ yay -Sy brave-bin --noconfirm
 yay -Sy visual-studio-code-bin --noconfirm
 yay -Sy powershell-bin --noconfirm
 yay -Sy enpass-bin --noconfirm
-yay -Sy onedrive-abraunegg --noconfirm
 sudo pacman -Sy rclone --noconfirm
 sudo pacman -Sy gemini-cli --noconfirm
 sudo pacman -Sy dotnet-sdk aspnet-runtime --noconfirm
@@ -11,14 +10,17 @@ sudo pacman -S krusader --noconfirm
 sudo pacman -S vlc --noconfirm
 sudo pacman -S kompare --noconfirm
 sudo pacman -S krename --noconfirm
+sudo pacman -S signal-desktop --noconfirm
 export PATH="$PATH:/home/radek/.dotnet/tools"
+
 
 # Docker desktop
 wget https://download.docker.com/linux/static/stable/x86_64/docker-29.2.0.tgz -qO- | tar xvfz - docker/docker --strip-components=1
 sudo cp -rp ./docker /usr/local/bin/ && rm -r ./docker
 cd ~/Stažené
-wget https://desktop.docker.com/linux/main/amd64/217644/docker-desktop-x86_64.pkg.tar.zst
-sudo pacman -U ./docker-desktop-x86_64.pkg.tar.zst
+wget https://desktop.docker.com/linux/main/amd64/218231/docker-desktop-x86_64.pkg.tar.zst
+sudo pacman -U ./docker-desktop-x86_64.pkg.tar.zst --noconfirm
+rm ./docker-desktop-x86_64.pkg.tar.zst
 cd ~
 
 yay -S rider --noconfirm
@@ -27,4 +29,3 @@ yay -S rider --noconfirm
 rclone config
 mkdir -p /home/radek/Dokumenty/OneDrive
 rclone mount OneDrive: /home/radek/Dokumenty/OneDrive --vfs-cache-mode writes --daemon
-
